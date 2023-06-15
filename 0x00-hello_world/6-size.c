54cb5fec18eaff02ef56490ce9ea03d3b512148b
outmane <otman.jail10@gamil.com>
2023-06-15 12:47:19 -0700

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,14 +2,14 @@
 /**
  * main - Entry point
  * description:size of types in c :D
- * Return: Always 0 (Success)
+ * *Return: Always 0 (Success)
 */
 int main(void)
 {
-	printf("Size of char: %i byte(s)\n", sizeof(char));
-	printf("Size of an  int: %i byte(s)\n", sizeof(int));
-	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
-	printf("Size of long long int : %i byte(s)\n", sizeof(long long int));
-	printf("Size of float: %i byte(s)\n", sizeof(float));
+	printf("Size of char: %u byte(s)\n", sizeof(char));
+	printf("Size of an int: %u byte(s)\n", sizeof(int));
+	printf("Size of long int: %u byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %u byte(s)\n", sizeof(long long int));
+	printf("Size of float: %u byte(s)\n", sizeof(float));
 	return (0);
 }
