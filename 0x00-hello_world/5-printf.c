9dcf2b33ffe091292d69535898f08bd09d2498bb
outmane <otman.jail10@gamil.com>
2023-06-15 10:30:17 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* description: my first C program in alx using printif and its so cool to using it
+* *Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return 0;
+}
