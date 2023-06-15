123718dac3a2c6a9293a04c830f996ea10367e8c
outmane <otman.jail10@gamil.com>
2023-06-15 10:22:31 -0700

    my first apk

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,10 @@
  * main - Entry point
  * description: my first c program in alx using puts
  * *Return: Always 0 (Success)
- */
+*/
+
 int main(void)
 {
-      	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
