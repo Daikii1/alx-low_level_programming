9af34d7d9047004c12d4857024c64b82998302d9
outmane <otman.jail10@gamil.com>
2023-06-16 11:10:11 -0700

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * usinng for loop and if to make hexadicimal program in c
+ * Return: Always 0 (Success)
+*/
+#include <stdio.h>
+
+int main(void)
+{
+
+	int DG;
+
+	for (DG = 48; DG <= 57; DG++)
+	{
+		putchar(DG);
+	}
+	for (DG = 97; DG <= 102; DG++)
+	{
+		putchar(DG);
+	}
+	putchar('\n');
+	return (0);
