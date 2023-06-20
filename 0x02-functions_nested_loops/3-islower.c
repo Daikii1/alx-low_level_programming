e23cf94afc99fa828ebdf5eb974522022d58d754
outmane <otman.jail10@gamil.com>
2023-06-20 08:41:35 -0700

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - its a function to Verify the character is he lowercase
+ * @c: input of the fucnition in ascii
+ * Return: return 1 for lowercase and 0 if its not
+*/
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	return (0);
+	_putchar('n\')
+	}
+}
