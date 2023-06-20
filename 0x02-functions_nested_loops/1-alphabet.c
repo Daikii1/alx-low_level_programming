74467a6c1404575f85bcfa67d17cdfcaa3805381
outmane <otman.jail10@gamil.com>
2023-06-20 14:28:46 -0700

    alphabet lower

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet letters  in lower
+ *
+ *
+ */
+void print_alphabet(void)
+{
+	char LT;
+
+	for (LT = 'a'; lT <= 'z'; LT++)
+	{
+		_putchar(LT);
+	}
+	_putchar('\n');
+}
