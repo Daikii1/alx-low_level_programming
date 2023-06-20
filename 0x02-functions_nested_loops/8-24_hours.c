c2fe2f57b0107512ab7ed015c7a608bc3889fe16
outmane <otman.jail10@gamil.com>
2023-06-20 10:21:41 -0700

    last digit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * _islower - its a function to Verify the character is he lowercase
+ * @c: input of the fucnition in ascii
+ * Return: return 1 for lowercase and 0 if its not
+*/
+
