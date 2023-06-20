103ad4b4a55ede2bfb0505f14528139b75808f72
outmane <otman.jail10@gamil.com>
2023-06-20 16:51:46 -0700

    alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int LF = 0;
+	char LT = 'a';
+
+	while (LF < 10)
+	{
+		LT = 'a';
+		while (LT <= 'z')
+		{
+			_putchar(LT);
+			lt++;
+		}
+		_putchar('\n');
+
+		LF++;
+	}
+}
