f3d2ee2ccae208d12b612accfe0cc40e61aadd54
outmane <otman.jail10@gamil.com>
2023-06-20 16:46:28 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - letters
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char LT = 'a';
+
+	while (LT <= 'z')
+	{
+		_putchar(letter);
+		LT++;
+	}
+	_putchar('\n');
+}
