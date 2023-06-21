1d131bf7fe2ab5bc045791e7e975fb20d2f895cb
outmane <otman.jail10@gamil.com>
2023-06-20 17:02:40 -0700

    alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha -  function
+ * @c: the input to verfiy 
+ * Return: 1 when is letter
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
