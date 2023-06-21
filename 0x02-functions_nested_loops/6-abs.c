f28e740f9e6457696f6fa94f7f4a89ca0129f26f
outmane <otman.jail10@gamil.com>
2023-06-20 17:16:56 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - function that give the absolute valute of number
+ * @nb: Nb input
+ * Return: Absolute value of number
+ */
+int _abs(int nb)
+{
+	if (nb >= 0)
+	{
+		return (nb);
+	}
+	else
+	{
+		return (nb);
+	}
+}
