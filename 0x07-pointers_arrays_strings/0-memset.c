ecd33ca859820198eda0fcdb6449ab8ee7853e26
outmane <otman.jail10@gamil.com>
2023-07-03 11:24:08 -0700

    :D

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * *_memset - a fucntion the fills memory with a constant byte
+ * *_memset - a fucntion fills memory with a constant byte.
  * @s: pointer
  * @b: constant byte
- * @n: number of buffer spaces to be fill
+ * @n: number of buffer spaces to be filled
  * Return: s
  */
 char *_memset(char *s, char b, unsigned int n)
