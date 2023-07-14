934ee16b56744611286a892858554681a2fefa8b
outmane <otman.jail10@gamil.com>
2023-07-14 05:37:07 -0700

    str

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+/**
+ * _strdup - Returns a pointer to a newly-allocated space in memory
+ *           containing a copy of the string given as parameter.
+ * @str: The string to be copied.
+ * Return: dp
+ */
+char *_strdup(char *str)
+{
+	char *dp;
+	int cpt, ln = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (cpt = 0; str[cpt]; cpt++)
+		len++;
+
+	dp = malloc(sizeof(char) * (ln + 1));
+
+	if (dp == NULL)
+		return (NULL);
+
+	for (cpt = 0; str[cpt]; cpt++)
+		dp[cpt] = str[cpt];
+
+	dp[ln] = '\0';
+
+	return (dp);
+}
