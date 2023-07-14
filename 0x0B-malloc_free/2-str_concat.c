9ea44aae0fa9b6f2f76608869daf075903f45a5e
outmane <otman.jail10@gamil.com>
2023-07-14 06:20:45 -0700

    str

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,36 +2,41 @@
 #include <stdlib.h>
 
 /**
- * str_concat - Concatenates two strings.
- * @s1: The string to be concatenated upon.
- * @s2: The string to be concatenated to s1.
- *
- * Return: cap_str
+ * str_concat - concatenates two strings.
+ * @s1: the first string.
+ * @s2: the second string.
+ * Return: pointr of an array of chars
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *cap_str;
-	int cpt, cap_cpt = 0, len = 0;
+	char *stro;
+	unsigned int i, j, k, cpt;
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
 
-	for (cpt = 0; s1[cpt] || s2[cpt]; cpt++)
-		len++;
+	for (i = 0; s1[i] != '\0'; i++)
+		;
 
-	cap_str = malloc(sizeof(char) * len);
+	for (j = 0; s2[j] != '\0'; j++)
+		;
 
-	if (cap_str == NULL)
+	stro = malloc(sizeof(char) * (i + j + 1));
+
+	if (stro == NULL)
+	{
+		free(stro);
 		return (NULL);
+	}
 
-	for (cpt = 0; s1[cpt]; cpt++)
-		cap_str[cap_cpt++] = s1[cpt];
+	for (k = 0; k < i; k++)
+		stro[k] = s1[k];
 
-	for (cpt = 0; s2[cpt]; cpt++)
-		cap_str[cap_cpt++] = s2[cpt];
+	cpt = j;
+	for (j = 0; j <= cpt; k++, j++)
+		stro[k] = s2[j];
 
-	return (cap_str);
+	return (stro);
 }
