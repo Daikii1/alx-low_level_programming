b2c24263ce2dcd8b3800a22c22a3d6bc857007fa
outmane <otman.jail10@gamil.com>
2023-07-14 17:06:36 -0700

    array

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * array_range - creates an array of integers
+ * @min: minimum
+ * @max: maximum
+ * Return: array
+ */
+int *array_range(int min, int max)
+{
+	int *ttr, i = 0, j = min;
+
+	if (min > max)
+		return (0);
+	ttr = malloc((max - min + 1) * sizeof(int));
+
+	if (!ttr)
+		return (0);
+	while (i <= max - min)
+		ttr[i++] = j++;
+	return (ttr);
+}
