3e76c746084eb4952dd664450412a4044505bc76
outmane <otman.jail10@gamil.com>
2023-07-20 03:13:35 -0700

    print

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings.
+ * @separator: string that will printed
+ * @n: number of strings
+ *
+ * Return: none
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int cpt;
+	char *s;
+
+	va_start(valist, n);
+
+	for (cpt = 0; cpt < n; i++)
+	{
+		s = va_arg(valist, char *);
+
+		if (s)
+			printf("%s", s);
+		else
+			printf("(nil)");
+
+		if (cpt < n - 1)
+			if (separator)
+				printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(valist);
+}
