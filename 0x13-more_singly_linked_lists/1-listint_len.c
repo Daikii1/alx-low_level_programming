c481f28207060a98260158cf45d94fe106a6c982
outmane <otman.jail10@gamil.com>
2023-07-31 08:23:17 -0700

    1-listint

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,13 +1,14 @@
 #include "lists.h"
 
 /**
- * listint_len -its a function that returns the number of elementsin a linked list
+ * listint_len -its a function that returns
+ * the number of elementsin a linked list
  * @h: adresse to the first element.
  * Return: number of elements.
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t cpt= 0;
+	size_t cpt = 0;
 
 	while (h)
 	{
