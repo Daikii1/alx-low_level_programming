0f0845423d068b5c2ec2cc57098ad664076af7be
outmane <otman.jail10@gamil.com>
2023-08-03 07:19:52 -0700

    print bin

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,21 +3,18 @@
 /**
  * print_binary - prints the binary representation
  * of a The Number.
- * @n: 	The unsigned long int input.
- *
+ * @n:The unsigned long int input.
  * Return: No Return.
  */
 void print_binary(unsigned long int n)
 {
 	if (n >> 0)
 	{
-		if (n >> 1)
-		{
-		print_binary(n >> 1);
-		_putchar((n & 1) + '0');
-		}
+	if (n >> 1)
+	print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 	}
 	else
-		_putchar('0');
-	
+	_putchar('0');
+
 }
