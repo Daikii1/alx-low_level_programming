7e2b32e0f72580a53710d5d2a037f382101dd0ac
outmane <otman.jail10@gamil.com>
2023-08-07 08:06:44 -0700

    fixing

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,7 +8,7 @@
  * @argv: The Arguments vector.
  * Return: No return.
  */
-void error_file(int file_from, int file_to, char *argv[])
+void err_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
 	{
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
 
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(file_from, file_to, argv);
+	err_file(file_from, file_to, argv);
 
 	N_chars = 1024;
 	while (N_chars == 1024)
