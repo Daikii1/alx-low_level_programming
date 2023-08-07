1821193f1776a4f5c61eaba695ff79a2df6ddf27
outmane <otman.jail10@gamil.com>
2023-08-07 04:14:41 -0700

    append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	Op = open(filename, O_WRONLY | O_APPEND);
-	wr = write(Op, text_content, len);
+	wr = write(Op, text_content, cpt);
 
 	if (Op == -1 || wr == -1)
 		return (-1);
